b0c950480139c098e674584221529eb22b3ee07d
Arthur Henrique <arthurhenrique@live.com>
2020-08-20 17:29:22 -0300

    🐛 Checkout controller index should receive the product in which the user is trying to buy

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
 
 class CheckoutController extends Controller
 {
     /**
      * Renderiza a página de checkout
+     * 
+     * @param   \App\Product
+     * @return  \Illuminate\View\View
      */
-    public function index()
+    public function index(Product $product)
     {
         return view('checkouts.simple');
     }
